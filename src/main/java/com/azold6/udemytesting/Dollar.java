c9fa0f78480b7af670c3818a195df59cb58ebb41
Alex <alex.rodrigues23997@gmail.com>
2022-05-17 21:23:26 -0300

    Adding Dollar and testing

---
+++ src/main/java/com/azold6/udemytesting/Dollar.java
@@ -0,0 +1,14 @@
+package com.azold6.udemytesting;
+
+public class Dollar {
+
+    int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    void times( int multiplier) {
+        amount *= multiplier;
+    }
+}
