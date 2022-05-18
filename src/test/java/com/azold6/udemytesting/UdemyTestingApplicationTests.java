38ca143ffce98569c26738b4b90766a72fbfa11c
Alex <alex.rodrigues23997@gmail.com>
2022-05-17 21:11:46 -0300

    Initial commit

---
+++ src/test/java/com/azold6/udemytesting/UdemyTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azold6.udemytesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
